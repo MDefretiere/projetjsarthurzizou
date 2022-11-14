50c423cb2469f4104a69ae5b5ba812b24bb250d3
MattDef1274 <defretierematthieu@gmail.com>
2022-11-14 20:46:56 +0100

    tt

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 $ampoule = document.getElementById("ampoule");
 
+$stade = document.getElementById("stade");
+
+$cardeurs = document.getElementById("cardeurs");
+
+$saintevictoire = document.getElementById("saintevictoire");
+
+$calanque = document.getElementById("calanque");
+
+$zizoukarim = document.getElementById("zizoukarim");
+
+$cassis = document.getElementById("cassis");
+
+function click1(){
+	$stade.style.animation = "descentestade 4s both";
+};
+
+function click2(){
+	$cardeurs.style.animation = "descentecardeurs 4s both";
+};
+
+function click3(){
+	$saintevictoire.style.animation = "descentesaintevictoire 4s both";
+};
+
+function click4(){
+	$calanque.style.animation = "descentecalanque 4s both";
+};
+
+function click5(){
+	$zizoukarim.style.animation = "descentezizoukarim 4s both";
+};
+
+function click6(){
+	$cassis.style.animation = "descentecassis 4s both";
+};
